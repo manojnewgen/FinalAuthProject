64e59ebf73c2d86b73ab5cd7567f2c876abe2845
Manoj.kumar <mannu9058@gmail.com>
2025-05-19 22:33:59 +0530

    add  middle ware

---
+++ Middlewares/InputValidator.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+
+namespace FinalAuthProject.Middlewares
+{
+    public static class InputValidator
+    {
+        public static string SanitizeInput(string input)
+        {
+            // XSS koruması için HTML encoding
+            return HttpUtility.HtmlEncode(input);
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Middlewares/SecurityHeadersMiddleware.cs
@@ -0,0 +1,21 @@
+﻿namespace FinalAuthProject.Middlewares
+{
+    public class SecurityHeadersMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public SecurityHeadersMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+            context.Response.Headers.Add("X-Frame-Options", "DENY");
+            context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'");
+
+            await _next(context);
+        }
+    }
+}
